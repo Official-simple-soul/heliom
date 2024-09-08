c7339e245e62176ac7a5bd472466487a76240028
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2024-09-09 00:02:02 +0100

    ft-auth-flow

--- src/components/StatusModal.tsx
+++ src/components/StatusModal.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { FaCheck, FaTimes } from 'react-icons/fa'; // Import icons for success and error
+import { FaCheck, FaTimes } from 'react-icons/fa';
 
 interface StatusModalProps {
   statusText: string;
   statusDesc: string;
-  btnText?: string; // Optional to provide flexibility
-  btnColor?: string; // Optional, default color applied based on type
-  icon?: JSX.Element; // Optional icon that can be passed
+  btnText?: string;
+  btnColor?: string;
+  icon?: JSX.Element;
   onClick: () => void;
-  type?: 'success' | 'error'; // To determine the type of the modal
+  type?: 'success' | 'error';
 }
 
 const StatusModal: React.FC<StatusModalProps> = ({
@@ -18,13 +18,11 @@ const StatusModal: React.FC<StatusModalProps> = ({
   btnColor,
   icon,
   onClick,
-  type = 'success', // Default to 'success' if not provided
+  type = 'success',
 }) => {
-  // Default button text and color based on the type
   const defaultBtnText = type === 'success' ? 'Proceed to Profile' : 'Retry';
   const defaultBtnColor = type === 'success' ? 'bg-[#0F3CB1]' : 'bg-red-600';
 
-  // Default icon based on the type
   const defaultIcon =
     type === 'success' ? (
       <div className="bg-pri w-20 h-20 rounded-full flex items-center justify-center">
@@ -40,7 +38,7 @@ const StatusModal: React.FC<StatusModalProps> = ({
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50 flex items-center justify-center">
       <div className="bg-white rounded-lg p-8 shadow-lg w-full max-w-md">
         <div className="flex justify-center mb-4">
-          {icon ? icon : defaultIcon} {/* Use provided icon or default */}
+          {icon ? icon : defaultIcon}
         </div>
         <h2 className="text-2xl font-semibold text-center mb-2 text-black">
           {statusText}
