9c9d6116c640c23d21538b4f8ade45f03c05131e
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2024-09-22 13:40:08 +0100

    user-profile-

--- src/app/(account)/devices/register_device/components/InstallationDetails.jsx
+++ src/app/(account)/devices/register_device/components/InstallationDetails.jsx
@@ -1,27 +1,26 @@
-import { useState } from "react";
-import FormBuilder from "@/dynamics/FormBuilder";
-import { InstallationDetailsForm } from "../data/InstallationDetails";
-import { toast } from "react-toastify";
-import { validation } from "@/utils/validation";
+import { useState } from 'react';
+import FormBuilder from '@/dynamics/FormBuilder';
+import { InstallationDetailsForm } from '../data/InstallationDetails';
+import { toast } from 'react-toastify';
+import { validation } from '@/utils/validation';
+import { useGlobalContext } from '@/store/context';
 
 function InstallationDetails({ setActiveTab, isLastStep, handleBack }) {
   const [formData, setFormData] = useState();
-
+  const { setFormValues } = useGlobalContext();
   const [loadingState, setLoadingState] = useState(false);
 
   const handleSubmit = () => {
     if (!validation(formData).status) {
-      toast.error("Complete all form fields");
+      toast.error('Complete all form fields');
       return;
     }
     setLoadingState(true);
-    console.log("Info values:", formData);
+    setFormValues((prev) => ({ ...prev, ...formData }));
     setLoadingState(false);
     // Proceed to next tab
     setActiveTab();
   };
-
-  console.log("Form data", formData);
 
   return (
     <div className="bg-[#FFFFFF] p-10 rounded-[20px] flex flex-col justify-between min-h-[607px]">
@@ -44,7 +43,7 @@ function InstallationDetails({ setActiveTab, isLastStep, handleBack }) {
           onClick={handleSubmit}
           className="min-w-[188px] px-[16px] py-[8px] font-h-medium rounded-[8px] bg-pri text-[#FFFFFF]"
         >
-          {isLastStep ? "Submit" : "Next"}
+          {isLastStep ? 'Submit' : 'Next'}
         </button>
       </div>
     </div>
