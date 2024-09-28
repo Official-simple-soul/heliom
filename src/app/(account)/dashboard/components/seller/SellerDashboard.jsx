db3fd664b149cfd891f661417ff73f19e7ce59b3
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2024-09-28 19:53:04 +0100

    update dashboard

--- src/app/(account)/dashboard/components/seller/SellerDashboard.jsx
+++ src/app/(account)/dashboard/components/seller/SellerDashboard.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Gauge from '@/components/Gauge';
+import { gaugesData } from '../../data/gauge_data';
 
 function SellerDashboard() {
-  return <div>seller</div>;
+  const [counts, setCounts] = useState(Array(6).fill(0));
+
+  useEffect(() => {
+    const intervals = counts.map((_, index) =>
+      setInterval(() => {
+        setCounts((prevCounts) =>
+          prevCounts.map((count, i) =>
+            i === index ? Math.floor(Math.random() * 100) : count
+          )
+        );
+      }, 2000)
+    );
+
+    return () => intervals.forEach((interval) => clearInterval(interval));
+  }, [counts]);
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="grid grid-cols-3 gap-8">
+        {gaugesData.map((gauge, index) => (
+          <Gauge
+            key={index}
+            count={counts[index]}
+            text={gauge.text}
+            unit={gauge.unit}
+            subArcs={gauge.subArcs}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SellerDashboard;
